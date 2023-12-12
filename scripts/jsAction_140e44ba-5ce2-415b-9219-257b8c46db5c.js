72a4c50f4a636d73c7cfadf5898ad5611779ee51
huangbinocp <participantd@up-esi.com>
2023-12-12 14:37:45 +0100

    C3ML_Azure_Test_NR_V2
    C3ML_Azure_Test_NR_V2

---
+++ scripts/jsAction_140e44ba-5ce2-415b-9219-257b8c46db5c.js
@@ -0,0 +1,35 @@
+﻿// Javascript skeleton.
+// Edit and adapt to your needs.
+// The documentation of the NeoLoad Javascript API
+// is available in the appendix of the documentation.
+
+// Get variable value from VariableManager
+var popup_end_date = context.variableManager.getValue("I_Start_Date");
+if (popup_end_date==null) {
+        context.fail("Variable 'I_Start_Date' not found");
+}
+
+
+logger.debug("popup_end_date="+popup_end_date);
+
+var popup_start_date = context.variableManager.getValue("popup_start_date");
+if (popup_start_date==null) {
+        context.fail("Variable 'popup_start_date' not found");
+}
+
+logger.debug("popup_end_date="+popup_end_date);
+
+var start_time =context.variableManager.getValue("start_time");
+logger.debug("start_time="+start_time);
+
+var time = new Date()
+var end_time = Math.round(time.getTime() / 1000)
+logger.debug("end_time="+end_time);
+
+
+// Do some computation using the methods
+// you defined in the JS Library
+
+var diftime =end_time - start_time;
+
+logger.debug("Time for popup windows loading ="+ diftime );
